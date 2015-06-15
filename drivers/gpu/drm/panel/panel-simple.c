1366e1ccc07050dbd0b56bff261b1c6b74c194cd
Michael Krummsdorf <michael.krummsdorf@tq-group.com>
2015-06-15 12:16:36 +0200

    panel-simple: add support for auo m240htn01
    Signed-off-by: Michael Krummsdorf <michael.krummsdorf@tq-group.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -538,6 +538,29 @@ static const struct panel_desc auo_b133htn01 = {
 	},
 };
 
+static const struct drm_display_mode auo_m240htn01_mode = {
+	.clock = 71200,
+	.hdisplay = 1920,
+	.hsync_start = 1920 + 40,
+	.hsync_end = 1920 + 40 + 80,
+	.htotal = 1920 + 40 + 80 + 60,
+	.vdisplay = 1080,
+	.vsync_start = 1080 + 25,
+	.vsync_end = 1080 + 25 + 15,
+	.vtotal = 1080 + 25 + 15 + 10,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc auo_m240htn01 = {
+	.modes = &auo_m240htn01_mode,
+	.num_modes = 1,
+	.bpc = 6,
+	.size = {
+		.width = 531,
+		.height = 299,
+	},
+};
+
 static const struct drm_display_mode avic_tm070ddh03_mode = {
 	.clock = 51200,
 	.hdisplay = 1024,
@@ -1010,6 +1033,9 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "auo,b133xtn01",
 		.data = &auo_b133xtn01,
+	}, {
+		.compatible = "auo,m240htn01",
+		.data = &auo_m240htn01,
 	}, {
 		.compatible = "avic,tm070ddh03",
 		.data = &avic_tm070ddh03,
