efc1a031d4c8e482fbbcfc0ff2605530544d79e7
Fabio Estevam <fabio.estevam@freescale.com>
2015-10-28 10:20:30 -0200

    fec: Use gpio_set_value_cansleep()
    We are in a context where we can sleep, and the FEC PHY reset gpio
    may be on an I2C expander. Use the cansleep() variant when
    setting the GPIO value.
    
    Based on a patch from Russell King for pci-mvebu.c.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f4444574a41430315a9c3eeb9e37cb0a36444a35)
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>
    
    Conflicts:
    	drivers/net/ethernet/freescale/fec_main.c

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -3358,7 +3358,7 @@ static void fec_reset_phy(struct platform_device *pdev)
 		return;
 	}
 	msleep(msec);
-	gpio_set_value(phy_reset, 1);
+	gpio_set_value_cansleep(phy_reset, 1);
 	if (delay_msec)
 		msleep(delay_msec);
 }
