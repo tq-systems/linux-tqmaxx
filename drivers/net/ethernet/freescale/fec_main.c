06c035d2e6bd417fc131310a6522f85b13358078
Quentin Schulz <quentin.schulz@free-electrons.com>
2017-05-23 11:48:08 +0200

    net: fec: add post PHY reset delay DT property
    Some PHY require to wait for a bit after the reset GPIO has been
    toggled. This adds support for the DT property `phy-reset-post-delay`
    which gives the delay in milliseconds to wait after reset.
    
    If the DT property is not given, no delay is observed. Post reset delay
    greater than 1000ms are invalid.
    
    This reverts also commit 6e4004d1bcaa8ac2fa323ecd42c4a303f7f1bd75
    since this was an out of tree DT property.
    
    Signed-off-by: Quentin Schulz <quentin.schulz@free-electrons.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Acked-by: Fugang Duan <fugang.duan@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 159a07604a99bd01e7db112de08d53dc4fcad109)
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>
    
    Conflicts:
    	Documentation/devicetree/bindings/net/fsl-fec.txt
    	drivers/net/ethernet/freescale/fec_main.c

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -3330,8 +3330,7 @@ static int fec_enet_init(struct net_device *ndev)
 static int fec_reset_phy(struct platform_device *pdev)
 {
 	int err, phy_reset;
-	int msec = 1;
-	int delay_msec = 0;
+	int msec = 1, phy_post_delay = 0;
 	struct device_node *np = pdev->dev.of_node;
 
 	if (!np)
@@ -3342,16 +3341,16 @@ static int fec_reset_phy(struct platform_device *pdev)
 	if (!err && msec > 1000)
 		msec = 1;
 
-	of_property_read_u32(np, "phy-reset-delay", &delay_msec);
-	/* A sane reset delay should not be longer than 1s */
-	if (delay_msec > 1000)
-		delay_msec = 10;
-
 	phy_reset = of_get_named_gpio(np, "phy-reset-gpios", 0);
 	if (phy_reset == -EPROBE_DEFER)
 		return phy_reset;
 	else if (!gpio_is_valid(phy_reset))
 		return 0;
+
+	err = of_property_read_u32(np, "phy-reset-post-delay", &phy_post_delay);
+	/* valid reset duration should be less than 1s */
+	if (!err && phy_post_delay > 1000)
+		return -EINVAL;
 
 	err = devm_gpio_request_one(&pdev->dev, phy_reset,
 				    GPIOF_OUT_INIT_LOW, "phy-reset");
@@ -3362,8 +3361,15 @@ static int fec_reset_phy(struct platform_device *pdev)
 	msleep(msec);
 
 	gpio_set_value_cansleep(phy_reset, 1);
-	if (delay_msec)
-		msleep(delay_msec);
+
+	if (!phy_post_delay)
+		return 0;
+
+	if (phy_post_delay > 20)
+		msleep(phy_post_delay);
+	else
+		usleep_range(phy_post_delay * 1000,
+			     phy_post_delay * 1000 + 1000);
 
 	return 0;
 }
