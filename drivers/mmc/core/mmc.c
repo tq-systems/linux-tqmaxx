4219d2d0448e5996f29d2b294972e9e1ec83dea8
Markus Niebel <Markus.Niebel@tq-group.com>
2017-09-19 16:15:51 +0200

    mmc: add e-MMC 5.1 as supported rev.
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -331,7 +331,7 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 	 * are authorized, see JEDEC JESD84-B50 section B.8.
 	 */
 	card->ext_csd.rev = ext_csd[EXT_CSD_REV];
-	if (card->ext_csd.rev > 7)
+	if (card->ext_csd.rev > 8)
 		pr_warn("%s: unrecognised EXT_CSD revision %d\n",
 			mmc_hostname(card->host), card->ext_csd.rev);
 
