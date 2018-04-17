fdd635894f79a213913f9a9d433802d17d851e8c
Michael Krummsdorf <michael.krummsdorf@tq-group.com>
2018-04-17 08:50:48 +0200

    drm/fsl-dcu: Add HDMI driver for freescale DCU
    This patch creates another Encoder for HDMI port, and linking the Encoder
    to appropriate DRM bridge. And this Encoder using same CRTC with RGB-LCD.
    For RGB-LCD and HDMI using the same hardware connection to DCU, RGB-LCD
    panel should be unplugged when using the HDMI connection.
    
    Signed-off-by: Alison Wang <alison.wang@nxp.com>
    Signed-off-by: Xiubo Li <lixiubo@cmss.chinamobile.com>
    Signed-off-by: Jianwei Wang <jianwei.wang.chn@gmail.com>
    Signed-off-by: Meng Yi <meng.yi@nxp.com>
    Signed-off-by: Michael Krummsdorf <michael.krummsdorf@tq-group.com>

--- drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_kms.c
+++ drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_kms.c
@@ -17,10 +17,18 @@
 #include "fsl_dcu_drm_crtc.h"
 #include "fsl_dcu_drm_drv.h"
 
+static void fsl_dcu_drm_output_poll_changed(struct drm_device *dev)
+{
+	struct fsl_dcu_drm_device *fsl_dev = dev->dev_private;
+
+	drm_fbdev_cma_hotplug_event(fsl_dev->fbdev);
+}
+
 static const struct drm_mode_config_funcs fsl_dcu_drm_mode_config_funcs = {
 	.atomic_check = drm_atomic_helper_check,
 	.atomic_commit = drm_atomic_helper_commit,
 	.fb_create = drm_fb_cma_create,
+	.output_poll_changed = fsl_dcu_drm_output_poll_changed,
 };
 
 int fsl_dcu_drm_modeset_init(struct fsl_dcu_drm_device *fsl_dev)
@@ -42,6 +50,10 @@ int fsl_dcu_drm_modeset_init(struct fsl_dcu_drm_device *fsl_dev)
 	ret = fsl_dcu_drm_encoder_create(fsl_dev, &fsl_dev->crtc);
 	if (ret)
 		goto err;
+
+	ret = fsl_dcu_drm_hdmienc_create(fsl_dev, &fsl_dev->crtc);
+	if (ret)
+		DRM_ERROR("Failed to create HDMI encoder\n");
 
 	ret = fsl_dcu_create_outputs(fsl_dev);
 	if (ret)
