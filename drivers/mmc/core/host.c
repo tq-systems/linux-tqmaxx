7c06c99a61dfd6ebcfbe08db2c35965625f135cd
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2018-09-10 11:11:59 +0200

    mmc: host: allow setting index via devicetree
    As with gpio, uart and others, allow specifying the host index via the
    aliases-node in the devicetree.
    
    On embedded devices, there is often a combination of removable (e.g.
    SD card) and non-removable mmc devices (e.g. eMMC).
    Therefore the mmcblk name_idx might change depending on
    	- host of removable device
    	- removable card present or not
    
    This makes it difficult to hard code the root device, if it is on the
    non-removable device. E.g. if SD card is present eMMC will be mmcblk1,
    if SD card is not present at boot, eMMC will be mmcblk0.
    
    To match the host index with the mmcblk, allow setting the index via
    an alias node.
    
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -350,15 +350,20 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 {
 	int err;
 	struct mmc_host *host;
+	int alias_id;
 
 	host = kzalloc(sizeof(struct mmc_host) + extra, GFP_KERNEL);
 	if (!host)
 		return NULL;
 
+	alias_id = of_alias_get_id(dev->of_node, "mmc");
+	if (alias_id < 0)
+		alias_id = 0;
+
 	/* scanning will be enabled when we're ready */
 	host->rescan_disable = 1;
 
-	err = ida_simple_get(&mmc_host_ida, 0, 0, GFP_KERNEL);
+	err = ida_simple_get(&mmc_host_ida, alias_id, 0, GFP_KERNEL);
 	if (err < 0) {
 		kfree(host);
 		return NULL;
