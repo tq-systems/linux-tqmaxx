e32974a2eb44c7ceb637b06aff5dea5a16f6d5cb
Max Merchel <Max.Merchel@tq-group.com>
2019-02-14 13:40:12 +0100

    drm/panel:[HACK] Add support for the tianma tm070jvhg33
    Signed-off-by: Max Merchel <Max.Merchel@tq-group.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -2894,7 +2894,19 @@ static const struct panel_desc tianma_tm070jdhg30 = {
 		.width = 151,
 		.height = 95,
 	},
+
 	.bus_format = MEDIA_BUS_FMT_RGB888_1X7X4_SPWG,
+};
+
+static const struct panel_desc tianma_tm070jvhg33 = {
+	.timings = &tianma_tm070jdhg30_timing,
+	.num_timings = 1,
+	.bpc = 8,
+	.size = {
+		.width = 150,
+		.height = 94,
+	},
+	.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
 };
 
 static const struct display_timing tianma_tm070rvhg71_timing = {
@@ -3454,6 +3466,9 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "tianma,tm070jdhg30",
 		.data = &tianma_tm070jdhg30,
+	}, {
+		.compatible = "tianma,tm070jvhg33",
+		.data = &tianma_tm070jvhg33,
 	}, {
 		.compatible = "tianma,tm070rvhg71",
 		.data = &tianma_tm070rvhg71,
