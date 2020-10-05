7c97ab4ea80f6c8372eaa7d32f3e7efdd2172430
Vitaly Kuzmichev <vkuzmichev@dev.rtsoft.ru>
2020-10-05 14:59:36 +0300

    mtd: spi-nor: add support for Macronix MX66UM1G45G
    The MX66UM1G45G only supports single and octal mode. Switching the flash
    to octal mode is not implemented yet, so we don't set the support flag
    for now.
    
    Signed-off-by: Vitaly Kuzmichev <vkuzmichev@dev.rtsoft.ru>
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -2813,6 +2813,7 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "mx66u51235f", INFO(0xc2253a, 0, 64 * 1024, 1024, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
 	{ "mx66l1g45g",  INFO(0xc2201b, 0, 64 * 1024, 2048, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "mx66l1g55g",  INFO(0xc2261b, 0, 64 * 1024, 2048, SPI_NOR_QUAD_READ) },
+	{ "mx66um1g45g", INFO(0xc2803b, 0, 64 * 1024, 2048, SECT_4K | SPI_NOR_4B_OPCODES) },
 
 	/* Micron <--> ST Micro */
 	{ "n25q016a",	 INFO(0x20bb15, 0, 64 * 1024,   32, SECT_4K | SPI_NOR_QUAD_READ) },
