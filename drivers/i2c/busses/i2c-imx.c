92061840f8002912e21bdf4f6f259dd9512c0c96
Markus Niebel <Markus.Niebel@tq-group.com>
2021-01-29 17:52:40 +0100

    i2c: i2c-imx: fix lockdep warning / deadlock
    commit e8b8f8761f62 ("MLK-20368 i2c-imx: Coverity: fix divide by zero warning")
    brings in a call to i2c_imx_set_clk in i2c_imx_start. This results in
    clock_get_rate locking the global clock prepare_lock.
    
    If a clock driver is registered as i2c device or subdevice
    this can lead to deadlock since the new clock driver itself
    could hold the prepare_lock while doing i2c requests.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- drivers/i2c/busses/i2c-imx.c
+++ drivers/i2c/busses/i2c-imx.c
@@ -646,10 +646,6 @@ static int i2c_imx_start(struct imx_i2c_struct *i2c_imx)
 
 	dev_dbg(&i2c_imx->adapter.dev, "<%s>\n", __func__);
 
-	result = i2c_imx_set_clk(i2c_imx, clk_get_rate(i2c_imx->clk));
-	if (result)
-		return result;
-
 	imx_i2c_write_reg(i2c_imx->ifdr, i2c_imx, IMX_I2C_IFDR);
 	/* Enable I2C controller */
 	imx_i2c_write_reg(i2c_imx->hwdata->i2sr_clr_opcode, i2c_imx, IMX_I2C_I2SR);
