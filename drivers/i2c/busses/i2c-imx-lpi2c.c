366e96ec9064fb96a8007e711e7d1446028532f3
Markus Niebel <Markus.Niebel@tq-group.com>
2021-02-01 10:40:56 +0100

    i2c: lpi2c: use clk notifier for per clk rate changes
    Instead of repeatedly calling clk_get_rate for each transfer, register
    a clock notifier and store actual per_clk rate for later use. Prevent
    clk_get_rate calls inside i2c xfer to allow usage / registration of
    I2C devices that implement clock devices.
    
    This prevents deadlocks when one device triggered an xfer and another
    device holding the clk_prepare_lock when waiting for the i2c bus lock.
    
    Tested on i.MX8QXP board (TQMa8XQP on MBa8Xx)
    
    TODO: evaluate, if complete controller initialisation in each transmit
    can be left out as implemented in i2c-imx.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- drivers/i2c/busses/i2c-imx-lpi2c.c
+++ drivers/i2c/busses/i2c-imx-lpi2c.c
@@ -104,6 +104,8 @@ struct lpi2c_imx_struct {
 	int			irq;
 	struct clk		*clk_per;
 	struct clk		*clk_ipg;
+	struct notifier_block	clk_change_nb;
+	unsigned int		per_clk_rate;
 	void __iomem		*base;
 	__u8			*rx_buf;
 	__u8			*tx_buf;
@@ -219,17 +221,12 @@ static void lpi2c_imx_stop(struct lpi2c_imx_struct *lpi2c_imx)
 static int lpi2c_imx_config(struct lpi2c_imx_struct *lpi2c_imx)
 {
 	u8 prescale, filt, sethold, clkhi, clklo, datavd;
-	unsigned int clk_rate, clk_cycle;
+	unsigned int clk_rate = lpi2c_imx->per_clk_rate;
+	unsigned int clk_cycle;
 	enum lpi2c_imx_pincfg pincfg;
 	unsigned int temp;
 
 	lpi2c_imx_set_mode(lpi2c_imx);
-
-	clk_rate = clk_get_rate(lpi2c_imx->clk_per);
-	if (!clk_rate) {
-		dev_dbg(&lpi2c_imx->adapter.dev, "clk_per rate is 0\n");
-		return -EINVAL;
-	}
 
 	if (lpi2c_imx->mode == HS || lpi2c_imx->mode == ULTRA_FAST)
 		filt = 0;
@@ -275,6 +272,20 @@ static int lpi2c_imx_config(struct lpi2c_imx_struct *lpi2c_imx)
 		writel(temp, lpi2c_imx->base + LPI2C_MCCR0);
 
 	return 0;
+}
+
+static int lpi2c_imx_clk_notifier_call(struct notifier_block *nb,
+				       unsigned long action, void *data)
+{
+	struct clk_notifier_data *ndata = data;
+	struct lpi2c_imx_struct *lpi2c_imx = container_of(&ndata->clk,
+							  struct lpi2c_imx_struct,
+							  clk_per);
+
+	if (action & POST_RATE_CHANGE)
+		lpi2c_imx->per_clk_rate = clk_get_rate(lpi2c_imx->clk_per);
+
+	return NOTIFY_OK;
 }
 
 static int lpi2c_imx_master_enable(struct lpi2c_imx_struct *lpi2c_imx)
@@ -672,6 +683,14 @@ static int lpi2c_imx_probe(struct platform_device *pdev)
 	if (ret)
 		lpi2c_imx->bitrate = LPI2C_DEFAULT_RATE;
 
+	lpi2c_imx->per_clk_rate = clk_get_rate(lpi2c_imx->clk_per);
+	lpi2c_imx->clk_change_nb.notifier_call = lpi2c_imx_clk_notifier_call;
+	clk_notifier_register(lpi2c_imx->clk_per, &lpi2c_imx->clk_change_nb);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "can't get I2C per clock\n");
+		goto clk_notifier_unregister;
+	}
+
 	i2c_set_adapdata(&lpi2c_imx->adapter, lpi2c_imx);
 	platform_set_drvdata(pdev, lpi2c_imx);
 
@@ -690,17 +709,19 @@ static int lpi2c_imx_probe(struct platform_device *pdev)
 	ret = lpi2c_imx_init_recovery_info(lpi2c_imx, pdev);
 	/* Give it another chance if pinctrl used is not ready yet */
 	if (ret == -EPROBE_DEFER)
-		goto rpm_disable;
+		goto clk_notifier_unregister;
 
 	ret = i2c_add_adapter(&lpi2c_imx->adapter);
 	if (ret)
-		goto rpm_disable;
+		goto clk_notifier_unregister;
 
 	dev_info(&lpi2c_imx->adapter.dev, "LPI2C adapter registered\n");
 
 	return 0;
 
-rpm_disable:
+clk_notifier_unregister:
+	clk_notifier_unregister(lpi2c_imx->clk_per, &lpi2c_imx->clk_change_nb);
+
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_dont_use_autosuspend(&pdev->dev);
 
@@ -715,6 +736,8 @@ static int lpi2c_imx_remove(struct platform_device *pdev)
 
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_dont_use_autosuspend(&pdev->dev);
+
+	clk_notifier_unregister(lpi2c_imx->clk_per, &lpi2c_imx->clk_change_nb);
 
 	return 0;
 }
