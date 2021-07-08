f9cfb1cc720f9cb56e9f12ae568d44673d46a41b
Markus Niebel <Markus.Niebel@tq-group.com>
2021-07-08 17:49:35 +0200

    media: mxc_mipi_csi: improve style
    - use unsigned int for loop counter
    - use fsleep for short delay
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/media/platform/mxc/capture/mxc_mipi_csi.c
+++ drivers/media/platform/mxc/capture/mxc_mipi_csi.c
@@ -342,7 +342,7 @@ static inline struct csi_state
 
 static const struct csis_pix_format *find_csis_format(u32 code)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < ARRAY_SIZE(mipi_csis_formats); i++)
 		if (code == mipi_csis_formats[i].code)
@@ -365,7 +365,7 @@ static void mipi_csis_sw_reset(struct csi_state *state)
 	u32 val = mipi_csis_read(state, MIPI_CSIS_CMN_CTRL);
 
 	mipi_csis_write(state, MIPI_CSIS_CMN_CTRL, val | MIPI_CSIS_CMN_CTRL_RESET);
-	udelay(10);
+	fsleep(10);
 }
 
 static int mipi_csis_phy_init(struct csi_state *state)
@@ -636,7 +636,7 @@ static void mipi_csis_stop_stream(struct csi_state *state)
 static void mipi_csis_clear_counters(struct csi_state *state)
 {
 	unsigned long flags;
-	int i;
+	unsigned int i;
 
 	spin_lock_irqsave(&state->slock, flags);
 	for (i = 0; i < MIPI_CSIS_NUM_EVENTS; i++)
