eaef111c59fc4102d15476adb5389221dbd84e6c
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2021-08-04 14:32:27 +0200

    BSPIMX8M-231 media:mxc-mipi-csi2_yav: fixed s_stream()
    s_stream() can be called unsymmetrically; moved power management into
    open()/release() and just check whether streaming is already active or
    not.
    
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/media/platform/imx8/mxc-mipi-csi2_yav.c
+++ drivers/media/platform/imx8/mxc-mipi-csi2_yav.c
@@ -316,17 +316,30 @@ static int mipi_csi2_s_power(struct v4l2_subdev *sd, int on)
 {
 	struct mxc_mipi_csi2_dev *csi2dev = sd_to_mxc_mipi_csi2_dev(sd);
 	struct v4l2_subdev *sensor_sd = csi2dev->sensor_sd;
+	struct device *dev = &csi2dev->pdev->dev;
+	int ret;
 
 	if (!sensor_sd)
 		return -ENODEV;
 
-	return v4l2_subdev_call(sensor_sd, core, s_power, on);
+	if (on)
+		pm_runtime_get_sync(dev);
+
+	ret = v4l2_subdev_call(sensor_sd, core, s_power, on);
+	if ((ret < 0) && (ret != -ENOIOCTLCMD)) {
+		dev_warn(&csi2dev->pdev->dev, "sensor's s_power failed: %d\n", ret);
+		on = 0;
+	}
+
+	if (!on)
+		pm_runtime_put_sync(dev);
+
+	return ret;
 }
 
 static int mipi_csi2_s_stream(struct v4l2_subdev *sd, int enable)
 {
 	struct mxc_mipi_csi2_dev *csi2dev = sd_to_mxc_mipi_csi2_dev(sd);
-	struct device *dev = &csi2dev->pdev->dev;
 	struct v4l2_subdev *sensor_sd = csi2dev->sensor_sd;
 	int ret = 0;
 
@@ -334,28 +347,39 @@ static int mipi_csi2_s_stream(struct v4l2_subdev *sd, int enable)
 		__func__, enable, csi2dev->flags);
 
 	if (enable) {
-		if (!csi2dev->running) {
-			pm_runtime_get_sync(dev);
-			mxc_mipi_csi2_phy_reset(csi2dev);
-			mxc_mipi_csi2_hc_config(csi2dev);
-			mxc_mipi_csi2_enable(csi2dev);
-			mxc_mipi_csi2_reg_dump(csi2dev);
+		if (csi2dev->running) {
+			ret = -EBUSY;
+			goto out;
 		}
-		v4l2_subdev_call(sensor_sd, video, s_stream, true);
-		csi2dev->running++;
 
+		mxc_mipi_csi2_phy_reset(csi2dev);
+		mxc_mipi_csi2_hc_config(csi2dev);
+		mxc_mipi_csi2_enable(csi2dev);
+		mxc_mipi_csi2_reg_dump(csi2dev);
+
+		ret = v4l2_subdev_call(sensor_sd, video, s_stream, true);
+		if (ret < 0) {
+			mxc_mipi_csi2_disable(csi2dev);
+			goto out;
+		}
+
+		csi2dev->running = true;
 	} else {
+		if (!csi2dev->running) {
+			ret = -EPIPE;
+			goto out;
+		}
+
 		/* Mask interrupt */
 		writel(0x1ff, csi2dev->base_regs + 0x110);
 
-		v4l2_subdev_call(sensor_sd, video, s_stream, false);
-		csi2dev->running--;
-		if (!csi2dev->running) {
-			pm_runtime_put(dev);
-			mxc_mipi_csi2_disable(csi2dev);
-		}
+		ret = v4l2_subdev_call(sensor_sd, video, s_stream, false);
+
+		mxc_mipi_csi2_disable(csi2dev);
+		csi2dev->running = false;
 	}
 
+out:
 	return ret;
 }
 
