63a1fdf48dd1a54a04a23133ef5ccb843280f05b
Nadezda Lutovinova <lutovinova@ispras.ru>
2021-08-19 18:48:18 +0300

    usb: dwc3: imx8mp: request irq after initializing dwc3
    If IRQ occurs between calling  devm_request_threaded_irq() and
    initializing dwc3_imx->dwc3, then null pointer dereference occurs
    since dwc3_imx->dwc3 is used in dwc3_imx8mp_interrupt().
    
    The patch puts registration of the interrupt handler after
    initializing of neccesery data.
    
    Found by Linux Driver Verification project (linuxtesting.org).
    
    Reviewed-by: Fabio Estevam <festevam@gmail.com>
    Acked-by: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Nadezda Lutovinova <lutovinova@ispras.ru>
    Link: https://lore.kernel.org/r/20210819154818.18334-1-lutovinova@ispras.ru
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 6a48d0ae01a6ab05ae5e78328546a2f5f6d3054a)

--- drivers/usb/dwc3/dwc3-imx8mp.c
+++ drivers/usb/dwc3/dwc3-imx8mp.c
@@ -200,13 +200,6 @@ static int dwc3_imx8mp_probe(struct platform_device *pdev)
 	}
 	dwc3_imx->irq = irq;
 
-	err = devm_request_threaded_irq(dev, irq, NULL, dwc3_imx8mp_interrupt,
-					IRQF_ONESHOT, dev_name(dev), dwc3_imx);
-	if (err) {
-		dev_err(dev, "failed to request IRQ #%d --> %d\n", irq, err);
-		goto disable_clk;
-	}
-
 	pm_runtime_set_active(dev);
 	pm_runtime_enable(dev);
 	err = pm_runtime_get_sync(dev);
@@ -233,6 +226,13 @@ static int dwc3_imx8mp_probe(struct platform_device *pdev)
 		goto depopulate;
 	}
 	of_node_put(dwc3_np);
+
+	err = devm_request_threaded_irq(dev, irq, NULL, dwc3_imx8mp_interrupt,
+					IRQF_ONESHOT, dev_name(dev), dwc3_imx);
+	if (err) {
+		dev_err(dev, "failed to request IRQ #%d --> %d\n", irq, err);
+		goto depopulate;
+	}
 
 	device_set_wakeup_capable(dev, true);
 	pm_runtime_put(dev);
