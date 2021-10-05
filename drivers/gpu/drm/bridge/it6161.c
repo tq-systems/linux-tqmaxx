a2f48eef112952ce5bfeec5ceda4af6919cd857c
Alexander Stein <alexander.stein@ew.tq-group.com>
2021-10-05 10:17:51 +0200

    drm: bridge: it6161: Add missing include
    devm_gpiod_get_optional() needs linux/gpio/consumer.h header
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/gpu/drm/bridge/it6161.c
+++ drivers/gpu/drm/bridge/it6161.c
@@ -13,6 +13,7 @@
 #include <linux/err.h>
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
+#include <linux/gpio/consumer.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/pm_runtime.h>
