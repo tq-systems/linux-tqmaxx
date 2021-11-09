627c7d094991161655724b335249e91f21a4041a
Alexander Stein <alexander.stein@ew.tq-group.com>
2021-11-09 11:31:34 +0100

    spi: lpspi: release requested DMA channels
    The requested DMA channels are never released. Do this in .remove as well
    as in .probe. spi_register_controller() can return -EPROBE_DEFER if
    cs-gpios are not probed yet.
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>
    Link: https://lore.kernel.org/r/20211109103134.184216-1-alexander.stein@ew.tq-group.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit f02bff30114f385d53ae3e45141db602923bca5d)
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- drivers/spi/spi-fsl-lpspi.c
+++ drivers/spi/spi-fsl-lpspi.c
@@ -958,6 +958,8 @@ static int fsl_lpspi_remove(struct platform_device *pdev)
 	struct fsl_lpspi_data *fsl_lpspi =
 				spi_controller_get_devdata(controller);
 
+	fsl_lpspi_dma_exit(controller);
+
 	pm_runtime_disable(fsl_lpspi->dev);
 	return 0;
 }
