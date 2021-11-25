2aaab92a9195f06cc277520b407dd9f2f1fb2fbf
Alexander Stein <alexander.stein@ew.tq-group.com>
2021-11-25 08:59:09 +0100

    usb: dwc3: drd: Add support for usb-conn-gpio based usb-role-switch
    usb-conn-gpio devices are a subnode of the USB interface controller, which
    needs to be populated.
    This allows having a non-type-c connector providing dual-role.
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/usb/dwc3/drd.c
+++ drivers/usb/dwc3/drd.c
@@ -9,6 +9,7 @@
 
 #include <linux/extcon.h>
 #include <linux/of_graph.h>
+#include "linux/of_platform.h"
 #include <linux/platform_device.h>
 #include <linux/property.h>
 
@@ -571,6 +572,13 @@ static int dwc3_setup_role_switch(struct dwc3 *dwc)
 	dwc->role_sw = usb_role_switch_register(dwc->dev, &dwc3_role_switch);
 	if (IS_ERR(dwc->role_sw))
 		return PTR_ERR(dwc->role_sw);
+
+	/* populate connector entry */
+	ret = devm_of_platform_populate(dwc->dev);
+	if (ret) {
+		dev_err(dwc->dev, "DWC3 platform devices creation failed: %i\n", ret);
+		return ret;
+	}
 
 	dwc3_set_mode(dwc, mode);
 	return 0;
