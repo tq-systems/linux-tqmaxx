43401b677cae58816e914bcdbb63e7819e544f75
Nora Schiffer <nora.schiffer@ew.tq-group.com>
2022-03-09 15:43:00 +0100

    spi: spidev: add compatible string for NXP NAFE13388 analog input module
    Add new compatible to spi driver.
    
    Signed-off-by: Nora Schiffer <nora.schiffer@ew.tq-group.com>
    Signed-off-by: Alexander Feilke <Alexander.Feilke@ew.tq-group.com>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -718,6 +718,7 @@ static const struct spi_device_id spidev_spi_ids[] = {
 	{ .name = /* semtech */ "sx1301" },
 	{ .name = /* silabs */ "em3581" },
 	{ .name = /* silabs */ "si3210" },
+	{ .name = /* nxp */ "nafe13388" },
 	{},
 };
 MODULE_DEVICE_TABLE(spi, spidev_spi_ids);
@@ -751,6 +752,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "semtech,sx1301", .data = &spidev_of_check },
 	{ .compatible = "silabs,em3581", .data = &spidev_of_check },
 	{ .compatible = "silabs,si3210", .data = &spidev_of_check },
+	{ .compatible = "nxp,nafe13388", .data = &spidev_of_check },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
