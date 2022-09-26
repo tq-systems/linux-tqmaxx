521613e72cf9e3d554aaf764ba201ad0db187b2c
Alexander Stein <alexander.stein@ew.tq-group.com>
2022-09-26 11:20:51 +0200

    staging: media: imx8-media-dev: Fix probe if no sensors probed yet
    If no sensors are available (yet) and no links could be established,
    the probe function just returns success, while cleaning up.
    If no sensors are connected, defer probing to the point where sensors
    are successfully probed.
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/staging/media/imx/imx8-media-dev.c
+++ drivers/staging/media/imx/imx8-media-dev.c
@@ -1120,6 +1120,10 @@ static int mxc_md_probe(struct platform_device *pdev)
 				mxc_md_unregister_all(mxc_md);
 				v4l2_async_nf_unregister(&mxc_md->subdev_notifier);
 				v4l2_async_nf_cleanup(&mxc_md->subdev_notifier);
+				ret = dev_err_probe(&mxc_md->pdev->dev,
+						    -EPROBE_DEFER,
+						    "No sensors connected\n");
+				goto clean_ents;
 			}
 		}
 	}
