b7331b5424e1babc9d5294f78fe01d42270f6878
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2023-01-25 17:04:12 +0100

    mtd: spi-nor: diagnostic message for selected command set
    Useful for bringup to see exactly what will be used to access
    the flash. Ported from current U-Boot branch, was also available
    in older TQMa8 specific branches of TQ-Systems.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -3222,6 +3222,9 @@ int spi_nor_scan(struct spi_nor *nor, const char *name,
 	/* Configure OTP parameters and ops */
 	spi_nor_otp_init(nor);
 
+	dev_info(dev, "addr_width = %d erase_cmd = %x rd_cmd = %x pp_cmd = %x\n",
+		 (int)nor->addr_width, (u32)nor->erase_opcode,
+		 (u32)nor->read_opcode, (u32)nor->program_opcode);
 	dev_info(dev, "%s (%lld Kbytes)\n", info->name,
 			(long long)mtd->size >> 10);
 
