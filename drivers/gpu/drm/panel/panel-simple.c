05ef57d8de9440cf92a1b3bfb8cfa35d18691ee4
Alexander Stein <alexander.stein@ew.tq-group.com>
2023-08-14 16:15:30 +0200

    [HACK] drm: panel-simple: Add missing connector_type for edt_etm0700g0bdh6_24bit
    The connector_type was left out while porting this to newer kernels.
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -1786,6 +1786,7 @@ static const struct panel_desc edt_etm0700g0bdh6_24bit = {
 	},
 	.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
 	.bus_flags = DRM_BUS_FLAG_DE_HIGH | DRM_BUS_FLAG_PIXDATA_DRIVE_POSEDGE,
+	.connector_type = DRM_MODE_CONNECTOR_DPI,
 };
 
 static const struct display_timing edt_etml0700y5dha_timing = {
