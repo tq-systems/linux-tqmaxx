263ea4be06179c38715f9cd2e0a672c453748ce1
Alexander Stein <alexander.stein@ew.tq-group.com>
2025-04-23 09:23:37 +0200

    staging: fsl_ppfe: Fix build_skb usage
    frag_size of 0 means the memory is allocated using kmalloc, which is
    deprecated since commit ce098da1497c6 ("skbuff: Introduce slab_build_skb()")
    Use slab_build_skb() as indicated by the warning.
    Fixes the WARN_ON:
    Use slab_build_skb() instead
    WARNING: CPU: 0 PID: 0 at net/core/skbuff.c:418 __build_skb_around+0xe0/0xe8
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/staging/fsl_ppfe/pfe_eth.c
+++ drivers/staging/fsl_ppfe/pfe_eth.c
@@ -2129,7 +2129,7 @@ static struct sk_buff *pfe_eth_rx_skb(struct net_device *ndev,
 
 		/* First frag */
 		if (desc_ctrl & CL_DESC_FIRST) {
-			skb = build_skb(buf_addr, 0);
+			skb = slab_build_skb(buf_addr);
 			if (unlikely(!skb))
 				goto pkt_drop;
 
@@ -2151,7 +2151,7 @@ static struct sk_buff *pfe_eth_rx_skb(struct net_device *ndev,
 				goto pkt_drop;
 			}
 
-			skb_frag = build_skb(buf_addr, 0);
+			skb_frag = slab_build_skb(buf_addr);
 
 			if (unlikely(!skb_frag)) {
 				kfree(buf_addr);
