d939cde2bc851b7c59f1dcba871033851130ea10
Alexander Stein <alexander.stein@ew.tq-group.com>
2026-04-17 11:30:56 +0200

    Revert "drm/tilcdc: avoid WARN_ON() in drm_atomic_helper_shutdown() call"
    This reverts commit 031d5adcb9ba7babe624bb6f4ce323d13ead41df.
    Upstream has fixed the issue with deferred probes already in commit
    71be8825e83c9 ("drm/tilcdc: Fix removal actions in case of failed probe").
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/gpu/drm/tilcdc/tilcdc_drv.c
+++ drivers/gpu/drm/tilcdc/tilcdc_drv.c
@@ -175,10 +175,7 @@ static void tilcdc_fini(struct drm_device *dev)
 	drm_dev_unregister(dev);
 
 	drm_kms_helper_poll_fini(dev);
-
-	if (priv->is_registered)
-		drm_atomic_helper_shutdown(dev);
-
+	drm_atomic_helper_shutdown(dev);
 	tilcdc_irq_uninstall(dev);
 	drm_mode_config_cleanup(dev);
 
